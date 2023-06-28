3e85cb54e51971700211a378e209499d70f94701
Adjiedjiewhy <Adjie.wahyuw@gmail.com>
2023-06-28 21:50:46 +0700

    Added react-router-dom

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import anilistClient from "../clients/anilistClient";
 import "../styles/App.css";
