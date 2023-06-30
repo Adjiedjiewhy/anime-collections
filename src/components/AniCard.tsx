00c988dd1ee224c3e85a824efd4329de7f306473
Adjiedjiewhy <Adjie.wahyuw@gmail.com>
2023-06-30 11:47:16 +0700

    collection wip

---
+++ src/components/AniCard.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { useEffect } from "react";
+import { Primary } from "../styles/variables/colors";
+
+const AniCard: React.FC<any> = ({ cardTitle, cardImage }) => {
+  return (
+    <Card>
+      <CardImage src={cardImage} alt="Card Image 1" />
+      <CardTitle>
+        {cardTitle}
+        {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. */}
+      </CardTitle>
+    </Card>
+  );
+};
+
+const Card = styled.div`
+  position: relative;
+  background-color: #f2f2f2;
+  border-radius: 8px;
+  overflow: hidden;
+  width: 90%;
+  max-width: 280px;
+  margin: 0 auto;
+  
+  transition: 0.25s;
+  &:hover {
+    box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.25);
+    transform: scale(1.025);
+  }
+
+  @media (min-width: 768px) {
+    width: 300px;
+  }
+
+  @media (max-width: 767px) {
+    width: calc(50% - 8px); /* Two cards per row with 16px gap */
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 25%;
+    background: linear-gradient(to bottom, transparent, black 75%);
+    z-index: 1;
+  }
+`;
+
+const CardImage = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 0;
+`;
+
+const CardTitle = styled.div`
+  position: absolute;
+  bottom: 12px;
+  left: 12px;
+  right: 12px;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+  z-index: 2;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export default AniCard;
--- src/components/ColleList.tsx
+++ src/components/ColleList.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { Primary } from "../styles/variables/colors";
+import AniCard from "./AniCard";
 
 function ColleList() {
   const [cards] = useState([
@@ -11,7 +12,7 @@ function ColleList() {
     enim ad minim veniam, quis nostrud exercitation ullamco laboris
     nisi ut aliquip ex ea commodo consequat.`,
       image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
     },
     {
       title: "Card-2a",
@@ -20,157 +21,114 @@ function ColleList() {
       enim ad minim veniam, quis nostrud exercitation ullamco laboris
       nisi ut aliquip ex ea commodo consequat.`,
       image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
     },
     {
-      title: "Card-3a",
+      title: "Card-1a",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat.`,
+      image:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
+    },
+    {
+      title: "Card-2a",
       text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
       enim ad minim veniam, quis nostrud exercitation ullamco laboris
       nisi ut aliquip ex ea commodo consequat.`,
       image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
     },
     {
-      title: "Card-4a",
+      title: "Card-1a",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat.`,
+      image:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
+    },
+    {
+      title: "Card-2a",
       text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
       enim ad minim veniam, quis nostrud exercitation ullamco laboris
       nisi ut aliquip ex ea commodo consequat.`,
       image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
     },
     {
-      title: "Card-5a",
+      title: "Card-1a",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat.`,
+      image:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
+    },
+    {
+      title: "Card-2a",
       text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
       enim ad minim veniam, quis nostrud exercitation ullamco laboris
       nisi ut aliquip ex ea commodo consequat.`,
       image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
     },
     {
-      title: "Card-6a",
+      title: "Card-1a",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat.`,
+      image:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
+    },
+    {
+      title: "Card-2a",
       text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
       enim ad minim veniam, quis nostrud exercitation ullamco laboris
       nisi ut aliquip ex ea commodo consequat.`,
       image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
     },
     {
-      title: "Card-1b",
+      title: "Card-1a",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat.`,
+      image:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
+    },
+    {
+      title: "Card-2a",
       text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
       enim ad minim veniam, quis nostrud exercitation ullamco laboris
       nisi ut aliquip ex ea commodo consequat.`,
       image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
-    },
-    {
-      title: "Card-2b",
-      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat.`,
-      image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
-    },
-    {
-      title: "Card-3b",
-      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat.`,
-      image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
-    },
-    {
-      title: "Card-4b",
-      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat.`,
-      image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
-    },
-    {
-      title: "Card-5b",
-      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat.`,
-      image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
-    },
-    {
-      title: "Card-6b",
-      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat.`,
-      image:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx21507-BICFCVZY3xQb.jpg",
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx16498-C6FPmWm59CyP.jpg",
     },
   ]);
   return (
-    <div>
-      <Section>
-        <Container>
-          <CardList>
-            {cards.map((card, i) => (
-              <Card key={i}>
-                <Title>{card.title}</Title>
-                <p>{card.text}</p>
-              </Card>
-            ))}
-          </CardList>
-        </Container>
-      </Section>
-    </div>
+    <CardListContainer>
+      {cards.map((card, i) => (
+        <AniCard cardTitle={card.title} cardImage={card.image}/>
+      ))}
+    </CardListContainer>
   );
 }
 
-const Section = styled.div`
-  min-height: 100hv;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Container = styled.div`
-  max-width: 1300px;
-  width: 100%;
-  margin: auto;
-  padding: 0px;
-`;
-
-const CardList = styled.div`
+const CardListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Card = styled.div`
-  flex: 0 0 calc(33.33% - 20px);
-  max-width: 0 0 calc(33.33% - 20px);
-  width: 100%;
-  background-color: ${Primary.foreground};
-  border-radius: 10px;
-  transition 0.3s;
-  border: 2px solid transparent;
-  margin: 5px;
-
-  &:hover {
-    border: 2px solid;
-    border-color: ${Primary.base};
-  }
-`;
-
-const Title = styled.h3`
-  padding: 0px;
-  margin: 0px;
+  gap: 16px;
+  justify-content: center;
+  margin: 10px;
 `;
 
 export default ColleList;
